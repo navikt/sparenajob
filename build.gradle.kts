850503536fd1dc5ced6d3eeb7af2a5a8a725c073
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2023-05-04 18:44:41 +0200

    Update sevral dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,20 +6,20 @@ group = "no.nav.syfo"
 version = "1.0.0"
 
 val coroutinesVersion = "1.6.4"
-val jacksonVersion = "2.14.2"
+val jacksonVersion = "2.15.0"
 val kluentVersion = "1.72"
-val ktorVersion = "2.2.4"
+val ktorVersion = "2.3.0"
 val logbackVersion = "1.4.6"
 val logstashEncoderVersion = "7.3"
 val prometheusVersion = "0.16.0"
-val smCommonVersion = "1.9df1108"
+val smCommonVersion = "1.0.1"
 val mockkVersion = "1.12.5"
 val testContainerVersion = "1.17.3"
 val postgresVersion = "42.6.0"
 val flywayVersion = "9.0.4"
 val hikariVersion = "5.0.1"
 val googlePostgresVersion = "1.6.3"
-val kotlinVersion = "1.8.10"
+val kotlinVersion = "1.8.21"
 val kotestVersion = "5.4.1"
 
 tasks.withType<Jar> {
@@ -28,7 +28,7 @@ tasks.withType<Jar> {
 
 plugins {
     id("org.jmailen.kotlinter") version "3.10.0"
-    kotlin("jvm") version "1.8.10"
+    kotlin("jvm") version "1.8.21"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
