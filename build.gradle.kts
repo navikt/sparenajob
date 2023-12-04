7adf95dff3a622a8a7f48bfc2d29c8274ae1d06a
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2023-12-04 10:35:34 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,7 +65,8 @@ dependencies {
 
     implementation("org.postgresql:postgresql:$postgresVersion")
     implementation("com.zaxxer:HikariCP:$hikariVersion")
-    implementation("org.flywaydb:flyway-core:$flywayVersion")
+    compileOnly("org.flywaydb:flyway-core:$flywayVersion")
+    implementation("org.flywaydb:flyway-database-postgresql:$flywayVersion")
     implementation("com.google.cloud.sql:postgres-socket-factory:$googlePostgresVersion")
 
     implementation("com.fasterxml.jackson.module:jackson-module-jaxb-annotations:$jacksonVersion")
