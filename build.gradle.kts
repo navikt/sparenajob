1801f02ebc5f4e3f4d35ab9d9a85168607dd9e1d
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2024-12-09 13:47:15 +0100

    chore: bumped ktor to 3.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ version = "1.0.0"
 val coroutinesVersion = "1.9.0"
 val jacksonVersion = "2.18.2"
 val kluentVersion = "1.73"
-val ktorVersion = "3.0.1"
+val ktorVersion = "3.0.2"
 val logbackVersion = "1.5.12"
 val logstashEncoderVersion = "8.0"
 val prometheusVersion = "0.16.0"
@@ -19,7 +19,6 @@ val googlePostgresVersion = "1.21.0"
 val kotlinVersion = "2.1.0"
 val kotestVersion = "5.9.1"
 val ktfmtVersion = "0.44"
-val snappyJavaVersion = "1.1.10.7"
 val kafkaVersion = "3.9.0"
 val commonsCompressVersion = "1.27.1"
 val jvmVersion = JvmTarget.JVM_21
@@ -61,11 +60,6 @@ dependencies {
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
 
     implementation("org.apache.kafka:kafka_2.12:$kafkaVersion")
-    constraints {
-        implementation("org.xerial.snappy:snappy-java:$snappyJavaVersion") {
-            because("override transient from org.apache.kafka:kafka_2.12")
-        }
-    }
 
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
     implementation("net.logstash.logback:logstash-logback-encoder:$logstashEncoderVersion")
@@ -91,9 +85,7 @@ dependencies {
     testImplementation("org.amshove.kluent:kluent:$kluentVersion")
     testImplementation("io.mockk:mockk:$mockkVersion")
     testImplementation("org.testcontainers:kafka:$testContainerVersion")
-    testImplementation("io.ktor:ktor-server-test-host:$ktorVersion") {
-        exclude(group = "org.eclipse.jetty")
-    }
+    testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
 }
 
